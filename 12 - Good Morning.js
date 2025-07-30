da1b483b716e993ed6ed9574d05bb9eddbfea0b1
Sebastian Perez <a222203003@unison.mx>
2025-07-30 00:56:51 +0000

    Add files via upload

---
+++ 12 - Good Morning.js
@@ -0,0 +1,6 @@
+let hour = 11;
+if(hour < 12){
+  console.log("Good Morning!!")
+}else{
+  console.log("Good After-Morning :P")
+}
---
+++ 15 - Magic 8 Ball.js
@@ -0,0 +1,21 @@
+console.log("Should I continue using Codédex?")
+const rN = Math.floor(Math.random() * 9) + 1; 
+if(rN == 1){
+  console.log("Yes - definitely.")
+}else if(rN == 2){
+  console.log("It is decidedly so.")
+}else if(rN == 3){
+  console.log("Reply hazy, try again.")
+}else if(rN == 4){
+  console.log("Ask again later.")
+}else if(rN == 5){
+  console.log("Better not tell you now.")
+}else if(rN == 6){
+  console.log("My sources say no.")
+}else if(rN == 7){
+  console.log("Outlook not so good.")
+}else if(rN == 8){
+  console.log("Very doubtful.")
+}else{
+  console.log("Without a doubt.")
+}
