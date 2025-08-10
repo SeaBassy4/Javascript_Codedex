f6b5077bf34dd1b972fb0d9b50822cc7acdc717c
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2025-08-10 15:04:49 -0700

    41 - Hotline Bling

---
+++ Final Chapter/41_Hotline_Bling/script.js
@@ -0,0 +1,5 @@
+let drakePicTwo = document.getElementById("drake-pic-2").querySelector("img".src);
+let heading = document.getElementById("heading-2").querySelector("h1");
+
+document.getElementById("drake-pic-2").src = "https://i.imgur.com/RGbh6zY.png";
+heading = document.getElementById("heading-2").innerText = "Learn by Doing"
